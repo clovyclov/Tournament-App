8382a2c618c0c616c6a4aaf9eeaf8d2e78a38cc6
Clovis <shropshireclovis@gmail.com>
2017-10-24 20:28:44 -0400

    Added summaries for the MatchupModel's and PersonModel's fields.

--- TrackerLibrary/PersonModel.cs
+++ TrackerLibrary/PersonModel.cs
@@ -8,9 +8,24 @@ namespace TrackerLibrary
 {
     public class PersonModel
     {
+        /// <summary>
+        /// The first name of the person.
+        /// </summary>
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// The last name of the person.
+        /// </summary>
         public string LastName { get; set; }
+
+        /// <summary>
+        /// The person's email address.
+        /// </summary>
         public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// The person's phone number.
+        /// </summary>
         public string PhoneNumber { get; set; }
     }
 }
